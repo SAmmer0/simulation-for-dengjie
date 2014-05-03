f60813056124a72181041e4cd3dd9a52a5443612
sammer0 <howardleeh@gmail.com>
2014-05-03 15:55:52 +0800

    重构后修改版本，优化了日志打印函数，部分bug仍未修复，发现部分错误，有待解决

--- simulation.py
+++ simulation.py
@@ -85,7 +85,7 @@ def trade_begin(price_dict, buy_item, sell_item, beta_dict,
     in_trade = True         # 设置交易状态
 
     trade_log_list.append([trade_date])        # 写入交易日志列表
-    trade_log_list.append[-1].append(buy_item)
+    trade_log_list[-1].append(buy_item)
 
     return cost_this, new_trade_cost_total, trade_log_list, trade_state, in_trade
 
@@ -110,17 +110,15 @@ def trade_end(price_dict, trade_state,
     cost_this = (buy + sell) * trade_cost_rate + buy + trade_cost_this          # 计算整个完整交易的成本
     new_trade_cost_total = trade_cost_total + (buy + sell) * trade_cost_rate + buy         # 计算至今为止的交易成本
 
-    if buy_item == 'a':
-        trade_state['sabh'] = False
-    else:
-        trade_state['shba'] = False
+    trade_state['sabh'] = False     # 还原未交易的状态
+    trade_state['shba'] = False
     in_trade = False
 
-    last_trade_date = trade_log_list[-1][1]
+    last_trade_date = trade_log_list[-1][0]
     revenue_this = (price_dict[buy_item][last_trade_date] - price_dict[buy_item][trade_date]) *\
         trade_number * beta_dict[buy_item] +\
         (price_dict[sell_item][trade_date] - price_dict[sell_item][last_trade_date]) *\
-        trade_number * beta_dict[buy_item]                              # 计算本次完整交易的收益
+        trade_number * beta_dict[sell_item]                              # 计算本次完整交易的收益
     new_revenue_total = revenue_total + revenue_this        # 计算总的交易收益
     rate_of_revenue = revenue_this / cost_this * 100
 
@@ -131,41 +129,56 @@ def trade_end(price_dict, trade_state,
     return new_trade_cost_total, new_revenue_total, trade_log_list, trade_state, in_trade
 
 
-def print_log(output_path, total_trade_log_list,
-              cost_revenue_list, name_msg_list, file_name='trade_log'):
+def print_log_by_trade(output_path, trade_log_list, time_str, stock_price_dict,
+                       total_cost, total_revenue, name_msg, print_model='a+', file_name='trade_log'):
     '''打印交易日志，包含所有的组合的交易日志'''
-    os.chdir(output_path)
+    cwd = os.getcwd()
+    if not(cwd == output_path):
+        os.chdir(output_path)
     file_name_txt = file_name + '.txt'
-    output_file = open(file_name_txt, 'w')
+    output_file = open(file_name_txt, print_model)
 
-    # 计算收益率以及所有的交易完成之后的总收益和收益率
-    total_rev = .0
-    total_cost = .0
-    for ith_trade in range(len(cost_revenue_list)):
-        rate_of_revenue = cost_revenue_list[1] / cost_revenue_list[0] * 100
-        total_rev += cost_revenue_list[1]
-        total_cost += cost_revenue_list[0]
+    rate_of_rev = total_revenue / total_cost * 100      # 计算收益率
 
-    for ith_trade in range(len(total_trade_log_list)):
-        output_file.write(name_msg_list[ith_trade])
+    output_file.write(name_msg)
+    for ith_trade in trade_log_list:
         output_file.write('\n')
-        for trade_list in total_trade_log_list[ith_trade]:
-            try:
-                if trade_list[1] == 'a':
-                    output_file.write('建仓时间：%s， 买A卖H\n') % (trade_list[0])
-                    output_file.write('平仓时间： %s， 卖A买H，本次收益为：%f，收益率为：%f%%\n')\
-                        % (trade_list[2], trade_list[3], trade_list[4])
-                else:
-                    output_file.write('建仓时间：%s，买H卖A') % (trade_list[0])
-                    output_file.write('平仓时间： %s， 卖H买A，本次收益为：%f，收益率为：%f%%\n')\
-                        % (trade_list[2], trade_list[3], trade_list[4])
-            except IOError as err:
-                print 'Error, unable to write log' + str(err)
+        try:
+            if ith_trade[1] == 'a':
+                output_file.write('建仓时间：%s，买A卖H，买入价：%f，卖出价：%f\n'
+                                  % (time_str[ith_trade[0]], stock_price_dict['a'][ith_trade[0]],
+                                     stock_price_dict['h'][ith_trade[0]]))
+                output_file.write('平仓时间：%s，卖A买H，卖出价：%f，买入价：%f，本次收益为：%f，收益率为：%f%%\n'
+                                  % (time_str[ith_trade[2]], stock_price_dict['a'][ith_trade[2]],
+                                     stock_price_dict['h'][ith_trade[2]], ith_trade[3], ith_trade[4]))
+            else:
+                output_file.write('建仓时间：%s，买H卖A，买入价：%f，卖出价：%f\n'
+                                  % (time_str[ith_trade[0]], stock_price_dict['h'][ith_trade[0]],
+                                     stock_price_dict['a'][ith_trade[0]]))
+                output_file.write('平仓时间：%s，卖H买A，卖出价：%f，买入价：%f，本次收益为：%f，收益率为：%f%%\n'
+                                  % (time_str[ith_trade[2]], stock_price_dict['h'][ith_trade[2]],
+                                     stock_price_dict['a'][ith_trade[2]], ith_trade[3], ith_trade[4]))
+        except IOError as err:
+            print 'Error, unable to write log' + str(err)
 
-        output_file.write('''本次交易总收益为：%f，
-                            总的收益率为 %f%%/n''') % (cost_revenue_list[1], rate_of_revenue[ith_trade])
-        output_file.write('\n')
-    output_file.write('总收益率为: %f，总收益率为：%f%%/n') % (total_rev, total_rev / total_cost * 100)
+    output_file.write('本次交易总收益为：%f,总的收益率为 %f%%\n'
+                      % (total_revenue, rate_of_rev))
+
+    output_file.write('\n|' + '-'*10 + 'NEXT-TRADE' + '-'*10 + '|\n\n')
+    output_file.close()
+
+
+def print_log_total(output_path, total_cost, total_revenue, file_name='trade_log'):
+    '''用于打印整个交易总和最终所得到的收益和收益率'''
+    cwd = os.getcwd()
+    if not(cwd == output_path):
+        os.chdir(output_path)
+
+    rate_of_rev = total_revenue / total_cost * 100
+
+    file_name_txt = file_name + '.txt'
+    output_file = open(file_name_txt, 'a+')
+    output_file.write('交易总计收益为：%f，交易总计收益率为：%f%%\n' % (total_revenue, rate_of_rev))
     output_file.close()
 
 
@@ -220,34 +233,35 @@ def plot_trade_procedure(diff_msg, sd_msg, constrain,
 
     # 保存图片
     file_save = output_path + '\\' + name_msg + '.jpg'
-    pl.save(file_save)
+    pl.savefig(file_save)
 
 
-def trade_iter(input_path, output_path):
+def trade_iteration(input_path, output_path):
+    reload(sys)
     sys.setdefaultencoding('utf-8')
     xls_name_list = get_xls_file(input_path)
 
     # 初始化全局变量
     constrain = 3.0
-    trigger = 1.5
+    trigger = 0.9
     trade_num = 100
     rate_of_cost = 0.0004
-    name_msg_list = []
-    total_cost_revenue_list = []
+    cost = .0       # 所有交易的总成本
+    rev = .0        # 所有交易的总成本
 
     for xls_name in xls_name_list:      # 文件处理循环
         # 获取文件中的数据
         diff_msg, sd_msg, price_a, price_h, time_msg, name_msg, beta_msg = read_file(xls_name, input_path)
 
-        # 将交易的两只股票名加入列表，方便打印
-        name_msg_list.append(name_msg)
-
         # 计算价差序列除以标准差序列
         diff_div_sd_list = diff_div_sd(diff_msg, sd_msg)
-        # 将日期型数据转换为字符
+
+        # 将日期型数据转换为字符，并加入总时间列表，供打印日志使用
         time_str = trans_time(time_msg)
+
         # 设置beta系数字典
         beta_dict = {'a': 1, 'h': beta_msg}
+
         stocks_price_dict = {'a': price_a, 'h': price_h}
 
         # 初始化域变量
@@ -256,19 +270,20 @@ def trade_iter(input_path, output_path):
         cost_total = 0
         revenue_total = 0
         trade_log_list = []     # 用于记录交易记录
-        total_trade_log_list = []
         len_trade_day = len(time_str)       # 总的交易天数
 
         # 日期循环
         for ith_day in range(len_trade_day):
             if not in_trade:
-                if trigger < diff_div_sd_list[ith_day] < constrain:
+                if (ith_day != 0) and (diff_div_sd_list[ith_day - 1] > constrain or diff_div_sd_list[-1] < -constrain):
+                    continue
+                elif trigger < diff_div_sd_list[ith_day] < constrain:
                     cost_this, cost_total, trade_log_list, trade_state, in_trade = trade_begin(stocks_price_dict,
                                                                                                'a', 'h', beta_dict,
                                                                                                trade_num, cost_total,
                                                                                                trade_log_list,
                                                                                                rate_of_cost, ith_day)
-                elif -constrain < diff_div_sd_list < -trigger:
+                elif -constrain < diff_div_sd_list[ith_day] < -trigger:
                     cost_this, cost_total, trade_log_list, trade_state, in_trade = trade_begin(stocks_price_dict,
                                                                                                'h', 'a', beta_dict,
                                                                                                trade_num, cost_total,
@@ -286,17 +301,32 @@ def trade_iter(input_path, output_path):
                                                                                                  trade_log_list,
                                                                                                  ith_day,
                                                                                                  rate_of_cost)
-                elif diff_msg[ith_day] * diff_msg[ith_day - 1] <= 0:  # 判断价差曲线是否突破横轴
-                    cost_total, revenue_total, trade_log_list, trade_state, in_trade = trade_end(stocks_price_dict,
-                                                                                                 trade_state,
-                                                                                                 beta_dict,
-                                                                                                 trade_num,
-                                                                                                 cost_total,
-                                                                                                 cost_this,
-                                                                                                 revenue_total,
-                                                                                                 trade_log_list,
-                                                                                                 ith_day,
-                                                                                                 rate_of_cost)
+                elif diff_msg[ith_day] * diff_msg[ith_day - 1] <= 0:  # 判断价差曲线是否突破横轴，及其突破的方式
+                    judge_index = pow(diff_msg[ith_day], 2) - pow(diff_msg[ith_day - 1], 2)
+                    if judge_index >= 0:
+                        cost_total, revenue_total, trade_log_list, trade_state, in_trade = trade_end(stocks_price_dict,
+                                                                                                     trade_state,
+                                                                                                     beta_dict,
+                                                                                                     trade_num,
+                                                                                                     cost_total,
+                                                                                                     cost_this,
+                                                                                                     revenue_total,
+                                                                                                     trade_log_list,
+                                                                                                     ith_day,
+                                                                                                     rate_of_cost)
+                elif diff_msg[ith_day] * diff_msg[ith_day + 1] <= 0:  # 判断价差曲线是否突破横轴，及其突破的方式
+                    judge_index = pow(diff_msg[ith_day], 2) - pow(diff_msg[ith_day + 1], 2)
+                    if judge_index <= 0:
+                        cost_total, revenue_total, trade_log_list, trade_state, in_trade = trade_end(stocks_price_dict,
+                                                                                                     trade_state,
+                                                                                                     beta_dict,
+                                                                                                     trade_num,
+                                                                                                     cost_total,
+                                                                                                     cost_this,
+                                                                                                     revenue_total,
+                                                                                                     trade_log_list,
+                                                                                                     ith_day,
+                                                                                                     rate_of_cost)
                 elif diff_div_sd_list[ith_day] < -constrain or diff_div_sd_list[ith_day] > constrain:
                     # 判断价差序列除标准差序列的结果是否突破了波动的上下界
                     cost_total, revenue_total, trade_log_list, trade_state, in_trade = trade_end(stocks_price_dict,
@@ -312,13 +342,18 @@ def trade_iter(input_path, output_path):
                 else:
                     continue
 
-        total_trade_log_list.append(trade_log_list)     # 将日志列表加入总的列表方便一起打印
-        total_cost_revenue_list.append([cost_total, revenue_total])
         plot_trade_procedure(diff_msg, sd_msg, constrain, trigger, output_path, name_msg, time_str)     # 作图
-
-    print_log(output_path, total_trade_log_list, total_cost_revenue_list, name_msg_list)
+        if xls_name_list.index(xls_name) == 0:
+            print_log_by_trade(output_path, trade_log_list, time_str, stocks_price_dict,
+                               cost_total, revenue_total, name_msg, 'w+')
+        else:
+            print_log_by_trade(output_path, trade_log_list, time_str, stocks_price_dict,
+                               cost_total, revenue_total, name_msg)
+        cost += cost_total
+        rev += revenue_total
+    print_log_total(output_path, cost, rev)
 
 if __name__ == '__main__':
     path_input = r'C:\Users\Mercury\Desktop\stock\second'
     path_output = r'C:\Users\Mercury\Desktop\stock\second'
-    trade_iter(path_input, path_output)
+    trade_iteration(path_input, path_output)
